b8a74bb4756f61af24699d00dd903734f85d3357
obk6506 <obk6506@gmail.com>
2025-11-27 21:19:30 +0900

    Fix Puma config to use ENV instead of Rails constant

--- config/puma.rb
+++ config/puma.rb
@@ -39,4 +39,4 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] || Rails.env.production?
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] || ENV["RAILS_ENV"] == "production"
