09d6c59d761250ad472055599df7c41bb4c6d00e
obk6506 <obk6506@gmail.com>
2025-11-28 12:31:51 +0900

    Add image upload for Gemini multimodal analysis2

--- app/models/fortune_log.rb
+++ app/models/fortune_log.rb
@@ -1,6 +1,8 @@
 class FortuneLog < ApplicationRecord
-    has_neighbor :embedding # 벡터 검색용 (기존 코드)
-    
-    # [추가] 이 모델은 'image'라는 이름의 파일을 하나 가질 수 있다!
-    has_one_attached :image
-  end
+  # pgvector + neighbor 젬에서 제공하는 매크로는 has_neighbors (복수형) 이다.
+  # 단수형(has_neighbor)로 적으면 지금처럼 NoMethodError가 발생한다.
+  has_neighbors :embedding
+
+  # [추가] 이 모델은 'image'라는 이름의 파일을 하나 가질 수 있다!
+  has_one_attached :image
+end
